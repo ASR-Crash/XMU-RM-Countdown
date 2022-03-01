b3ce88d9ed14e536f705b477edc1d1a808dacfac
ASR-Crash <49263321+ASR-Crash@users.noreply.github.com>
2022-03-01 10:11:21 +0800

    update competing time

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -105,8 +105,8 @@
     simplyCountdown = function (elt, args) {
         var parameters = extend({
                 year: 2022,
-                month: 5,
-                day: 16,
+                month: 3,
+                day: 25,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
